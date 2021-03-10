712df8c7884d81190020ba49a18341a9a10a6bc6
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 12:06:13 +0100

    Update Karaoke.cpp

--- Karaoke.cpp
+++ Karaoke.cpp
@@ -3,3 +3,10 @@
 
 #include "Karaoke.h"
 
+/*  - Construction du Karaoké
+       - Construction des joueurs
+       - Choix de la chanson
+       - Lancement
+       - Appel updateScore
+       - Moyenne des scores
+       - Meilleur score */

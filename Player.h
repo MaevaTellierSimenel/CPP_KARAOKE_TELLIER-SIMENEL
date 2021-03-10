4ae606e80f872a0b278ecb854c03ad008d4cc042
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 10:03:29 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -11,6 +11,12 @@ class Player {
         int _bestscore4;
         int _bestscore5;
 
+        int _newscore1;
+        int _newscore2;
+        int _newscore3;
+        int _newscore4;
+        int _newscore5;
+
     public:
 
         void getName(std::string _name);
@@ -20,12 +26,11 @@ class Player {
         void getScore4(int _bestscore4);
         void getScore5(int _bestscore5);
 
-        void updateName(std::string _name);
-        void updateScore1(int _bestscore1);
-        void updateScore2(int _bestscore2);
-        void updateScore3(int _bestscore3);
-        void updateScore4(int _bestscore4);
-        void updateScore5(int _bestscore5);
+        void updateScore1(int _bestscore1,int _newscore1);
+        void updateScore2(int _bestscore2,int _newscore2);
+        void updateScore3(int _bestscore3,int _newscore3);
+        void updateScore4(int _bestscore4,int _newscore4);
+        void updateScore5(int _bestscore5,int _newscore5);
 
         Player();
         Player(std::string _name,int _bestscore);

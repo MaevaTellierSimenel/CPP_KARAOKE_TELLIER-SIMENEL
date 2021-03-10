f8b18a1fad7864c1da33c6e77f757a72c9364029
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 10:19:12 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -36,11 +36,51 @@ void Player::getScore5(int _bestscore5){
 //______________________________________________________________________________________
 
 void Player::updateScore1(int _bestscore1,int _newscore1){
-    (_newscore5) == 0;
+    (_newscore1) == 0;
     std::cout<<"Donnez votre nouveau score (entre 50 et 100) : ";
     cin>> _newscore1 ;
 
     if ((_newscore1)>=(_bestscore1));
         (_bestscore1) == (_newscore1);
+    return;
+}
+
+void Player::updateScore2(int _bestscore2,int _newscore2){
+    (_newscore2) == 0;
+    std::cout<<"Donnez votre nouveau score (entre 50 et 100) : ";
+    cin>> _newscore2 ;
+
+    if ((_newscore2)>=(_bestscore2));
+        (_bestscore2) == (_newscore2);
+    return;
+}
+
+void Player::updateScore3(int _bestscore3,int _newscore3){
+    (_newscore3) == 0;
+    std::cout<<"Donnez votre nouveau score (entre 50 et 100) : ";
+    cin>> _newscore3 ;
+
+    if ((_newscore3)>=(_bestscore3));
+        (_bestscore3) == (_newscore3);
+    return;
+}
+
+void Player::updateScore4(int _bestscore4,int _newscore4){
+    (_newscore4) == 0;
+    std::cout<<"Donnez votre nouveau score (entre 50 et 100) : ";
+    cin>> _newscore4 ;
+
+    if ((_newscore4)>=(_bestscore4));
+        (_bestscore4) == (_newscore4);
+    return;
+}
+
+void Player::updateScore5(int _bestscore5,int _newscore5){
+    (_newscore5) == 0;
+    std::cout<<"Donnez votre nouveau score (entre 50 et 100) : ";
+    cin>> _newscore5 ;
+
+    if ((_newscore5)>=(_bestscore5));
+        (_bestscore5) == (_newscore5);
     return;
 }

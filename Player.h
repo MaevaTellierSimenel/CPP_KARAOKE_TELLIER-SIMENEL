d193d0a0286634d8a126208d186182a6f0ec17e3
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 11:30:56 +0100

    Update Player.h

--- Player.h
+++ Player.h
@@ -5,7 +5,7 @@ class Player {
     private:
 
         std::string _name;
-        
+
         int _bestscore1;
         int _bestscore2;
         int _bestscore3;
@@ -21,11 +21,6 @@ class Player {
     public:
 
         void getName(std::string _name);
-        void getScore1(int _bestscore1);
-        void getScore2(int _bestscore2);
-        void getScore3(int _bestscore3);
-        void getScore4(int _bestscore4);
-        void getScore5(int _bestscore5);
 
         void updateScore1(int _bestscore1,int _newscore1);
         void updateScore2(int _bestscore2,int _newscore2);

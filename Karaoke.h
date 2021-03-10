2aa227a06e45e57c62c7a455d01afc0215fbc29b
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 10:37:44 +0100

    Update Karaoke.h

--- Karaoke.h
+++ Karaoke.h
@@ -1,2 +1,16 @@
 #include <string>
 
+class Karaoke {
+
+    private :
+
+        std::string _song1;
+        std::string _song2;
+        std::string _song3;
+        std::string _song4;
+        std::string _song5;
+
+    public:
+
+
+}

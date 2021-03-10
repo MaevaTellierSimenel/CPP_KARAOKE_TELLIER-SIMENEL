145484339287e58d9d633de898f2140442ff8f70
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 09:55:59 +0100

    Update Player.cpp

--- Player.cpp
+++ Player.cpp
@@ -5,4 +5,31 @@
 
 void Player::getName(std::string _name){
     std::cout<<"Votre pseudo est"<< _name <<"."<<std::endl;
+    return;
 }
+
+void Player::getScore1(int _bestscore1){
+    std::cout<<"Votre meilleur score sur 1 est de"<< _bestscore1 <<"points."<<std::endl;
+    return;
+}
+
+void Player::getScore2(int _bestscore2){
+    std::cout<<"Votre meilleur score sur 2 est de"<< _bestscore2 <<"points."<<std::endl;
+    return;
+}
+
+void Player::getScore3(int _bestscore3){
+    std::cout<<"Votre meilleur score sur 3 est de"<< _bestscore3 <<"points."<<std::endl;
+    return;
+}
+
+void Player::getScore4(int _bestscore4){
+    std::cout<<"Votre meilleur score sur 4 est de"<< _bestscore4 <<"points."<<std::endl;
+    return;
+}
+
+void Player::getScore5(int _bestscore5){
+    std::cout<<"Votre meilleur score sur 5 est de"<< _bestscore5 <<"points."<<std::endl;
+    return;
+}
+

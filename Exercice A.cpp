446df12c3c5dd10aef0ff7c10c70c71716ec7dea
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 10:46:45 +0100

    Create Exercice A.cpp

---
+++ Exercice A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Player.cpp"
+
+using namespace std;
+
+int main(){
+
+    //********** Exercice A **********
+
+    Player one ("Linda",0);
+    
+
+    //********** Exercice B **********
+
+}

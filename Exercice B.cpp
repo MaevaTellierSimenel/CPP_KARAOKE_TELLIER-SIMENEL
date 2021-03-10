3b4a818e9cb679d91de67ba78b80e176ce49b77e
Maeva Tellier-Simenel <maeva.tellier02@gmail.com>
2021-03-10 12:06:08 +0100

    Update Exercice B.cpp

--- Exercice B.cpp
+++ Exercice B.cpp
@@ -6,3 +6,13 @@
 #include "Karaoke.cpp"
 
 using namespace std;
+
+int main(){
+    /* - Construction du Karaoké
+       - Construction des joueurs
+       - Choix de la chanson
+       - Lancement
+       - Appel updateScore
+       - Moyenne des scores
+       - Meilleur score */
+}
